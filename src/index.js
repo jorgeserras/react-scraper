f798911b6f1c9784920211a91263dc55333d629c
Jorge Serras <caveiroserras@gmail.com>
2019-08-11 19:17:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Scraper extends React.Component {
+  render() {
+    return (
+      <div>Web sraper form react is currently a work in progress (WIP).</div>
+    );
+  }
+}
+export default Scraper;
